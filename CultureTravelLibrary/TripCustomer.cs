f1058fa3290bf405f277d99f837a5ec6bcdb3286
Rick0335 <Rick0335@edu.eal.dk>
2018-12-04 15:02:48 +0100

    Opdateret properties

--- CultureTravelLibrary/TripCustomer.cs
+++ CultureTravelLibrary/TripCustomer.cs
@@ -9,24 +9,45 @@ namespace CultureTravelLibrary
     public class TripCustomer
     {
         #region Properties
-        public string Name { get; set; }
+        // PERSON
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+        // TRIP COMPANION
         public string PlusOne { get; set; }
-        public string Address { get; set; }
+
+        // ADDRESS
+        public string StreetName { get; set; }
+        public string StreetNumber { get; set; }
         public string ZipCode { get; set; }
         public string City { get; set; }
+
+        // CONTACT_INFO
         public string PhoneNumber { get; set; }
         public string Email { get; set; }
+
+
+        // ROOMTYPE
         public string RoomType { get; set; }
+
+        // AIRPORT
         public string Airport { get; set; }
+
+        // PASSPORT
+        public string DateOfIssue { get; set; }
+        public string ExpireDate { get; set; }
+        public string DateOfBirth { get; set; }
         #endregion
 
         // Constructor -- applies parameters to the properties
-        public TripCustomer (string name, string plusOne, string address, string zipCode, string city,
+        public TripCustomer (string firstName, string lastName, string plusOne, string streetName, string streetNumber, string zipCode, string city,
                              string phoneNumber, string email, string roomType, string airport)
         {
-            Name = name;
+            FirstName = firstName;
+            LastName = lastName;
             PlusOne = plusOne;
-            Address = address;
+            StreetName = streetName;
+            StreetNumber = streetNumber;
             ZipCode = zipCode;
             City = city;
             PhoneNumber = phoneNumber;
