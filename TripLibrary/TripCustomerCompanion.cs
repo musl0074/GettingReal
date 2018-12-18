09c95db4facdc4f378ec152ca1e84adeea946f16
Rick0335 <Rick0335@edu.eal.dk>
2018-12-18 13:00:35 +0100

    Interfaces

--- TripLibrary/TripCustomerCompanion.cs
+++ TripLibrary/TripCustomerCompanion.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace TripLibrary
 {
-    public class TripCustomer : IPersonalInformation, IContactInformation, ITripInformation, IPassportInformation
+    public class TripCustomerCompanion : IPersonalInformation, IPassportInformation
     {
         public string FirstName { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
         public string LastName { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
@@ -14,10 +14,7 @@ namespace TripLibrary
         public string StreetNumber { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
         public string CityName { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
         public string ZipCode { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
-        public string PhoneNumber { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
-        public string Email { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
-        public string RoomType { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
-        public string AirportName { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+
         public string PassportNumber { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
         public string DateOfIssue { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
         public string DateOfBirth { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
