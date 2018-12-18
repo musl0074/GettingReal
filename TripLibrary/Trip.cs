a5425797e36c56ce03daabc151722ee931f7d6df
Rick0335 <Rick0335@edu.eal.dk>
2018-12-18 14:25:45 +0100

    refaktorering

--- TripLibrary/Trip.cs
+++ TripLibrary/Trip.cs
@@ -8,9 +8,17 @@ namespace TripLibrary
 {
     public class Trip
     {
+        List<TripCustomer> tripCustomers = new List<TripCustomer>();
+
         public string Id { get; set; }
         public string Name { get; set; }
         public string Date { get; set; }
+
+
+        public Trip()
+        {
+
+        }
 
         public Trip (string id, string name, string date)
         {
@@ -18,10 +26,26 @@ namespace TripLibrary
             this.Name = name;
             this.Date = date;
         }
+
         public Trip (string name, string date)
         {
             Name = name;
             Date = date;
+        }
+
+        
+        public void CreateTripCustomers (string id, string fullName, string addressInformation, string contactInformation)
+        {
+            TripCustomer tripCustomer = new TripCustomer(id, fullName, addressInformation, contactInformation);
+            tripCustomers.Add(tripCustomer);
+
+
+        }
+
+
+        public List<TripCustomer> ShowTripCustomers ()
+        {
+            return tripCustomers;
         }
     }
 }
--- TripLibrary/TripCustomer.cs
+++ TripLibrary/TripCustomer.cs
@@ -20,6 +20,38 @@ namespace TripLibrary
         public string AirportName { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
         public string PassportNumber { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
         public string DateOfIssue { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
-        public string DateOfBirth { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public string DateOfBirth { get; set; }
+        public string Id { get; set; }
+
+        // Properties to read from Database
+        public string FullName { get; set; }
+        public string AddressInformation { get; set; }
+        public string ContactInformation { get; set; }
+
+
+        public TripCustomer(string firstName, string lastName, string streetName, string streetNumber, string cityName,
+                            string zipCode, string phoneNumber, string email, string roomType, string airportName)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+            StreetName = streetName;
+            StreetNumber = streetNumber;
+            CityName = cityName;
+            ZipCode = zipCode;
+            PhoneNumber = phoneNumber;
+            Email = email;
+            RoomType = roomType;
+            AirportName = airportName;
+        }
+
+        public TripCustomer(string id, string fullName, string addressInformation, string contactInformation)
+        {
+            Id = id;
+            FullName = fullName;
+            AddressInformation = addressInformation;
+            ContactInformation = contactInformation;
+        }
+
     }
+
 }
